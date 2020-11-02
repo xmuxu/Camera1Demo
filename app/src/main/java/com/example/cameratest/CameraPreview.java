d9333a2746d2b2187b310cd4cdf658f6f52cba84
junsxu <junsxu@126.com>
2020-11-03 00:53:23 +0800

    add source code. camera can preview and take pic.
    Signed-off-by: junsxu <junsxu@126.com>

---
+++ app/src/main/java/com/example/cameratest/CameraPreview.java
@@ -0,0 +1,88 @@
+package com.example.cameratest;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "CameraPreview";
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+    }
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        Log.d(TAG, "surfaceCreated");
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+        Log.d(TAG, "surfaceDestroyed");
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+        Log.d(TAG, "surfaceChanged");
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        Camera.Parameters mCameraParameters = mCamera.getParameters();
+        Camera.Size prevSize = mCameraParameters.getPreviewSize();
+        List<Camera.Size> supportPreSize = mCameraParameters.getSupportedPreviewSizes();
+        Camera.Size picSize = mCameraParameters.getPictureSize();
+        List<Camera.Size> supportPicSizes = mCameraParameters.getSupportedPictureSizes();
+
+        Log.d(TAG, "prevSize " + prevSize.width + "x" + prevSize.height + ", picSize " + picSize.width + "x" + picSize.height);
+        Log.d(TAG, "supported preview sizes:");
+        for(Camera.Size size : supportPreSize)
+            Log.d(TAG, size.width + "x" + size.height);
+        Log.d(TAG, "supported picture sizes:");
+        for(Camera.Size size : supportPicSizes)
+            Log.d(TAG, size.width + "x" + size.height);
+
+        mCameraParameters.setPreviewSize(800, 600);
+        mCameraParameters.setPictureSize(1600, 1200);
+        mCamera.setParameters(mCameraParameters);
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cameratest/MainActivity.java
@@ -0,0 +1,206 @@
+package com.example.cameratest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "CameraTest_main";
+    private static final int PERMISSION_CAMERA_REQUEST_CODE = 0x00000012;
+    private static final int PERMISSION_WRITE_EXTERNAL_STORAGE = 0x00000013;
+    private static final int PERMISSION_READ_EXTERNAL_STORAGE = 0x00000014;
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private FrameLayout previewLayout;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.d(TAG, "onPictureTaken");
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+                Log.d(TAG, "store pic at " + pictureFile.toString());
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        checkPermissionAndCamera();
+        checkReadExternalStoragePermission();
+        checkWriteExternalStoragePermission();
+
+        previewLayout = (FrameLayout) findViewById(R.id.camera_preview);
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+
+            c = Camera.open(0); // attempt to get a Camera instance
+            Log.d(TAG, "open camera-------" + c);
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private void checkPermissionAndCamera() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
+                Manifest.permission.CAMERA);
+        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            //没有权限，申请权限。
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_CAMERA_REQUEST_CODE);
+        }
+    }
+
+    private void checkReadExternalStoragePermission() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            //没有权限，申请权限。
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    PERMISSION_READ_EXTERNAL_STORAGE);
+        }
+    }
+
+    private void checkWriteExternalStoragePermission() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            //没有权限，申请权限。
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSION_WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERMISSION_CAMERA_REQUEST_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //允许权限，有调起相机拍照。
+                //openCamera();
+            } else {
+                //拒绝权限，弹出提示框。
+                Toast.makeText(this,"拍照权限被拒绝", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private static File getOutputMediaFile(int type){
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory(), "MyCameraApp");
+
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d(TAG, "failed to create directory " + mediaStorageDir.getPath());
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else {
+            Log.d(TAG, "media type is wrong");
+            return null;
+        }
+        Log.d(TAG, "mediaFile " + mediaFile.toString());
+        return mediaFile;
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+        if (mCamera == null) {
+            mCamera = getCameraInstance();
+        }
+        Log.d(TAG, "mCamera = " + mCamera);
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        previewLayout.addView(mPreview);
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+        mCamera.release();
+        mCamera = null;
+    }
+}
