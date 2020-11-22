6fb18d6f755e8508a4837e46ee282b71e6bda1d8
junsxu <junsxu@126.com>
2020-11-22 23:26:42 +0800

    支持录像功能
    Signed-off-by: junsxu <junsxu@126.com>

--- app/src/main/java/com/example/cameratest/MainActivity.java
+++ app/src/main/java/com/example/cameratest/MainActivity.java
@@ -8,6 +8,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -23,14 +25,21 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+// 参照 https://developer.android.google.cn/guide/topics/media/camera
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "CameraTest_main";
     private static final int PERMISSION_CAMERA_REQUEST_CODE = 0x00000012;
     private static final int PERMISSION_WRITE_EXTERNAL_STORAGE = 0x00000013;
     private static final int PERMISSION_READ_EXTERNAL_STORAGE = 0x00000014;
+    private static final int PERMISSION_AUDIO_REQUEST_CODE = 0x00000014;
     private Camera mCamera;
     private CameraPreview mPreview;
+    private MediaRecorder mediaRecorder;
     private FrameLayout previewLayout;
+    private Button captureButton;
+    private Button videoButton;
+    private boolean isRecording = false;
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int MEDIA_TYPE_VIDEO = 2;
 
@@ -59,6 +68,41 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private boolean prepareVideoRecorder() {
+        mediaRecorder = new MediaRecorder();
+
+        // Step 1: Unlock and set camera to MediaRecorder
+        mCamera.unlock();
+        mediaRecorder.setCamera(mCamera);
+
+        // Step 2: Set sources
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        // Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
+        mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+
+        // Step 4: Set output file
+        mediaRecorder.setOutputFile(getOutputMediaFile(MEDIA_TYPE_VIDEO).toString());
+
+        // Step 5: Set the preview output
+        mediaRecorder.setPreviewDisplay(mPreview.getHolder().getSurface());
+
+        // Step 6: Prepare configured MediaRecorder
+        try {
+            mediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            Log.d(TAG, "IllegalStateException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            Log.d(TAG, "IOException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,10 +111,11 @@ public class MainActivity extends AppCompatActivity {
         checkPermissionAndCamera();
         checkReadExternalStoragePermission();
         checkWriteExternalStoragePermission();
+        checkPermissionOfAudio();
 
         previewLayout = (FrameLayout) findViewById(R.id.camera_preview);
         // Add a listener to the Capture button
-        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton = (Button) findViewById(R.id.button_capture);
         captureButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -80,8 +125,49 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        // Add a listener to the Capture button
+        videoButton = (Button) findViewById(R.id.button_video);
+        videoButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (isRecording) {
+                            Log.d(TAG, "stop recording");
+                            // stop recording and release camera
+                            mediaRecorder.stop();  // stop the recording
+                            releaseMediaRecorder(); // release the MediaRecorder object
+                            //mCamera.lock();         // take camera access back from MediaRecorder
+
+                            // inform the user that recording has stopped
+                            setCaptureButtonText(videoButton, "Start recording");
+                            isRecording = false;
+                        } else {
+                            // initialize video camera
+                            if (prepareVideoRecorder()) {
+                                Log.d(TAG, "start recording");
+                                // Camera is available and unlocked, MediaRecorder is prepared,
+                                // now you can start recording
+                                mediaRecorder.start();
+
+                                // inform the user that recording has started
+                                setCaptureButtonText(videoButton, "Stop recording");
+                                Toast.makeText(MainActivity.this, "start recording", Toast.LENGTH_SHORT);
+                                isRecording = true;
+                            } else {
+                                // prepare didn't work, release the camera
+                                releaseMediaRecorder();
+                                // inform user
+                            }
+                        }
+                    }
+                }
+        );
     }
 
+    private void setCaptureButtonText(Button mButton, String mString) {
+        mButton.setText(mString);
+    }
     /** Check if this device has a camera */
     private boolean checkCameraHardware(Context context) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
@@ -99,7 +185,7 @@ public class MainActivity extends AppCompatActivity {
         try {
 
             c = Camera.open(0); // attempt to get a Camera instance
-            Log.d(TAG, "open camera-------" + c);
+            Log.d(TAG, "open camera");
         }
         catch (Exception e){
             // Camera is not available (in use or does not exist)
@@ -115,6 +201,17 @@ public class MainActivity extends AppCompatActivity {
             //没有权限，申请权限。
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},
                     PERMISSION_CAMERA_REQUEST_CODE);
+        }
+    }
+
+    private void checkPermissionOfAudio() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
+                Manifest.permission.RECORD_AUDIO);
+        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            //没有权限，申请权限。
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},
+                    PERMISSION_AUDIO_REQUEST_CODE);
         }
     }
 
@@ -155,8 +252,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
         File mediaStorageDir = new File(Environment.getExternalStorageDirectory(), "MyCameraApp");
 
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+        // Create the storage directory if it does not exist
         if (! mediaStorageDir.exists()){
             if (! mediaStorageDir.mkdirs()){
                 Log.d(TAG, "failed to create directory " + mediaStorageDir.getPath());
@@ -167,14 +269,17 @@ public class MainActivity extends AppCompatActivity {
         // Create a media file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE){
+        if (type == MEDIA_TYPE_IMAGE) {
             mediaFile = new File(mediaStorageDir.getPath() + File.separator +
                     "IMG_"+ timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
         } else {
-            Log.d(TAG, "media type is wrong");
             return null;
         }
         Log.d(TAG, "mediaFile " + mediaFile.toString());
+
         return mediaFile;
     }
 
@@ -182,6 +287,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         Log.d(TAG, "onPause");
         super.onPause();
+        releaseMediaRecorder();       // if you are using MediaRecorder, release it first
+        releaseCamera();
+    }
+
+    private void releaseMediaRecorder() {
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();   // clear recorder configuration
+            mediaRecorder.release(); // release the recorder object
+            mediaRecorder = null;
+            mCamera.lock();           // lock camera for later use
+        }
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null){
+            mCamera.setPreviewCallback(null);
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
     }
 
     public Camera.PreviewCallback mPreviewCallbak = new Camera.PreviewCallback() {
@@ -199,18 +323,14 @@ public class MainActivity extends AppCompatActivity {
             mCamera = getCameraInstance();
         }
         Log.d(TAG, "mCamera = " + mCamera);
-        // Create our Preview view and set it as the content of our activity.
         mPreview = new CameraPreview(this, mCamera);
         previewLayout.addView(mPreview);
-        mCamera.setPreviewCallback(mPreviewCallbak);
+        //mCamera.setPreviewCallback(mPreviewCallbak);
     }
 
     @Override
     protected void onStop() {
         Log.d(TAG, "onStop");
         super.onStop();
-        mCamera.release();
-        mCamera.setPreviewCallback(null);
-        mCamera = null;
     }
 }
